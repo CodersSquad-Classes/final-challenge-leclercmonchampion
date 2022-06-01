7e2be82feee1d640e62d45c11c61f3c49c74cd24
Mathieu Neumar <mathieu.neumar@epita.fr>
2022-05-31 19:51:32 -0500

    read

--- read.c
+++ read.c
@@ -1,1 +1,42 @@
-//TODO
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+char m[418];
+
+void read_text(){
+	char ch;
+	FILE *f;
+	f = fopen("maps.txt","r");
+	if(f==NULL){
+		printf("Error opening file\n");
+		exit(1);
+	}
+	
+	int i = 0;
+	while((ch=fgetc(f))!=EOF && i<418){
+		if((int)ch == 48 || (int)ch==49 ||(int)ch==50){
+			m[i] = ch;
+			i+=1;
+		}
+	}
+
+	fclose(f);
+}
+
+void print_matrix(){
+	for(size_t i = 0 ; i<9 ; i++){
+        for(size_t j = 0 ; j<9 ; j++){
+           printf("%c",m[i*9+j]); 
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+	read_text();
+    print_matrix();
+}
+
