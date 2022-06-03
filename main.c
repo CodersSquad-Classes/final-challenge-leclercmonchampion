eab63c5f382741a1381a0a96d5b701246fece4e3
Alice CARIOU <alice.cariou@free.fr>
2022-06-03 18:27:37 +0200

    structs pacman & ghosts

---
+++ main.c
@@ -0,0 +1,25 @@
+struct coord
+{
+	int x;
+	int y;
+};
+
+struct ghost
+{
+	struct coord pos;
+	int vx;
+	int vy;
+};
+
+struct pacman
+{
+	struct coord pos;
+	int lives;
+	int vx;
+	int vy;
+};
+
+int main()
+{
+}
+
