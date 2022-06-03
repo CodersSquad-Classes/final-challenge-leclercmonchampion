d2d24ebf73b9cd49b820f0356e69b387ef47750f
Baptiste Perrier <baptiste.perrier@epita.fr>
2022-06-03 11:40:47 -0500

    Creating one ghost and one player

--- main.c
+++ main.c
@@ -7,19 +7,23 @@ struct coord
 struct ghost
 {
 	struct coord pos;
-	int vx;
-	int vy;
 };
 
 struct pacman
 {
 	struct coord pos;
 	int lives;
-	int vx;
-	int vy;
 };
 
 int main()
 {
+	struct pacman player;
+	player.pos.x = 9;
+	player.pos.y = 15;
+	player.lives = 1;
+	struct ghost orange;
+	orange.pos.x = 9;
+	orange.pos.y = 9;
+	
 }
 
