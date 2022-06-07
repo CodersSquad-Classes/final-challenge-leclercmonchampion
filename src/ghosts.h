1eca65f138d4503b14c02ce7d536dace9b83d551
Babouche007 <bat.perrier@free.fr>
2022-06-07 02:04:45 -0500

    Cleaning to swich to go

--- src/ghosts.h
+++
@@ -1,6 +0,0 @@
-#ifndef GHOSTS_H
-#define GHOSTS_H
-
-void move_ghost(Ghost *g, int *printed);
-
-#endif
--- src/movement.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOVEMENT_H
-#define MOVEMENT_H
-
-
-
-void movement(Pacman *pacman, int *printed);
-
-#endif
