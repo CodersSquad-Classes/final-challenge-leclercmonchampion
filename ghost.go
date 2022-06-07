28e6f867332f4f18bfd6165a6641368579bac138
Alice CARIOU <alice.cariou@free.fr>
2022-06-07 03:35:31 -0500

    .

--- ghost.go
+++ ghost.go
@@ -1,15 +1,64 @@
 package main
 
-type ghost struct {
-	position sprite
-	status   GhostStatus
+import "math/rand"
+
+var maps []string
+
+type pos struct {
+	x int
+	y int
 }
 
-type sprite struct {
-	row      int
-	col      int
-	startRow int
-	startCol int
+var ghosts []*pos
+
+func moveGhosts() {
+	for _, g := range ghosts {
+		direction := findDirection()
+		g.x, g.y = Move(g.x, g.y, direction)
+	}
 }
 
-type GhostStatus string
+func findDirection() string {
+	dir := rand.Intn(4)
+	move := map[int]string{
+		0: "UP",
+		1: "DOWN",
+		2: "RIGHT",
+		3: "LEFT",
+	}
+	return move[dir]
+}
+
+func Move(oldRow, oldCol int, dir string) (newRow, newCol int) {
+	newRow, newCol = oldRow, oldCol
+
+	switch dir {
+	case "UP":
+		newRow = newRow - 1
+		if newRow < 0 {
+			newRow = len(maps) - 1
+		}
+	case "DOWN":
+		newRow = newRow + 1
+		if newRow == len(maps) {
+			newRow = 0
+		}
+	case "RIGHT":
+		newCol = newCol + 1
+		if newCol == len(maps[0]) {
+			newCol = 0
+		}
+	case "LEFT":
+		newCol = newCol - 1
+		if newCol < 0 {
+			newCol = len(maps[0]) - 1
+		}
+	}
+
+	if maps[newRow][newCol] == '2' {
+		newRow = oldRow
+		newCol = oldCol
+	}
+
+	return
+}
