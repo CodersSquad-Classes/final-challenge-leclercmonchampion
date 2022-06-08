3eb3db22a78f13eecfb1a0bf363234b24eefff34
Mathieu-21 <mathieuneumar21@gmail.com>
2022-06-07 20:06:08 -0500

    adding coins decrementation

--- read.go
+++ read.go
@@ -26,3 +26,14 @@ func readMap(file string, map_ [399]int) ([399]int, error) {
 
 	return map_, nil
 }
+
+// func that count the numbers of coins
+func countCoins(map_ [399]int) int {
+	coins := 0
+	for _, n := range map_ {
+		if n == 1 {
+			coins++
+		}
+	}
+	return coins
+}
